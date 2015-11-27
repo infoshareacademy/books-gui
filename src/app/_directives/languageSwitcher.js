22eea9b3493b063b8420200c11b4e6fc5caa5501
jakubmatyka <jaqbec@gmail.com>
2015-11-27 15:00:11 +0100

    polish language as a current language

--- src/app/_directives/languageSwitcher.js
+++ src/app/_directives/languageSwitcher.js
@@ -5,7 +5,7 @@ angular.module('BooksGUI')
             templateUrl: '_directives/languageSwitcher.html',
             scope: {},
             controller: function ($scope, gettextCatalog) {
-                $scope.currentLanguage = 'en_GB';
+                $scope.currentLanguage = 'pl_PL';
 
                 $scope.changeLanguage = function () {
                     gettextCatalog.setCurrentLanguage($scope.currentLanguage);
